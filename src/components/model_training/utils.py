10b6520cb7ce66b3388f1f9fde601b323784e496
ldebele <lemidebele@gmail.com>
2024-07-04 09:36:01 +0300

    added k8s

---
+++ src/components/model_training/utils.py
@@ -0,0 +1,39 @@
+
+import matplotlib.pyplot as plt
+
+
+
+def plot_history(history, type, y):
+    """
+        Args:
+            history : List[float]         -> train and validation values
+            type : str                    -> evaluation type [loss, accuracy]
+            y : tuple(float, float)       -> y coordinate values (y limiter)
+    """
+
+    plt.figure(figsize=(5, 4))
+    plt.plot(history.history[type], label=f"Training {type}")
+    plt.plot(history.history[f'val_{type}'], label=f"Validation {type}")
+    plt.xlabel("Epoch")
+    plt.ylabel(type)
+    plt.ylim(y)
+    plt.title(f"Training vs. Validation {type.capitalize()}")
+    plt.grid(True)
+    plt.legend(loc='upper left')
+    plt.savefig('./outputs/{type}.jpg')
+
+
+def plot_learning_rate(history):
+    """
+        Args:
+            history : List[float]       -> 
+    """
+
+    plt.figure(figsize=(10, 6))
+    plt.semelogx(history.history['lr'], history.history['loss'])
+    plt.tick_params('both', length=10, width=1, which='both')
+    plt.xlabel("Learning Rate")
+    plt.ylabel("Loss")
+    plt.title("Learning Rate")
+    plt.grid(True)
+    plt.savefig('./outputs/Learning_rate.jpg')
