4af228ce778b5b1bd46a3b140c7810fc9f832da2
ldebele <lemidebele@gmail.com>
2024-07-12 15:28:20 +0300

    fix error in data_ingestion pipeline

--- src/components/data_ingestion/README.md
+++ src/components/data_ingestion/README.md
@@ -1,10 +1,15 @@
 
 
-Go to your kaggle account settings and select "Create New API Token". This will download a `kaggle.json` file containing your API credentials.
+Go to your kaggle account settings and select "Create New API Token". This will download a `kaggle.json` a file that contains your API credentials. Make a directoyr `.kaggle` at root `~` and place `kaggle.json` in that directory.
+```
+mkdir ~/.kaggle
+mv kaggle.json ~/.kaggle
+```
 
 
 ```
 docker run \
-    -v ~/.kaggle:/root/.kaggle 
-    data-ingestion airbus-ship-detection
+    -v ~/.kaggle:/root/.kaggle \
+    airbus-ship-detection-data-ingestion \
+    ./download_dataset.sh airbus-ship-detection competitions
 ```
--- src/components/data_ingestion/download_dataset.sh
+++ src/components/data_ingestion/download_dataset.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+DATASET_NAME=$1
+TYPE=$2
+
 # check if the user provided a dataset
 if [ -z "$1" ]; then 
-    echo "Dataset path: Airbus-ship-detection"
+    echo "Usage: $0 <kaggle-dataset-path> <type>"
+    echo "Example: $0 airbus-ship-detection competitions"
     exit 1
 fi
-
-DATASET_NAME=$1
 
 # check if kaggle.json file exist
 if [ ! -f ~/.kaggle/kaggle.json ]; then
@@ -14,9 +16,22 @@ if [ ! -f ~/.kaggle/kaggle.json ]; then
     exit 1
 fi
 
+
+download_dataset() {
+    if [ "$TYPE" == "datasets" ]; then
+        kaggle datasets download -d $DATASET_NAME -p ./data --unzip
+    elif [ "$TYPE" == "competitions" ]; then
+        kaggle competitions download -c $DATASET_NAME -p ./data --unzip
+    else
+        echo "Invalid type. please specify 'datasets' or 'competitions'."
+        exit 1
+    fi
+}
+
 # download the dataset
 echo "Downloading dataset: $DATASET_NAME"
-kaggle competitions c download $DATASET_NAME -p ./ --unzip
+download_dataset
+
 
 if [ $? -eq 0 ]; then
     echo "Dataset downloaded successfully."
