35230db29fbfba068a9879d8795f18f4d1e1f4e3
ldebele <lemidebele@gmail.com>
2024-07-18 13:32:04 +0300

    added custom datagenerator class

--- src/components/preprocessing/preprocess.py
+++ src/components/preprocessing/preprocess.py
@@ -3,9 +3,11 @@ import logging
 import argparse
 from typing import Tuple
 
+import cv2 as cv
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+from tensorflow.keras.utils import Sequence
 
 from sklearn.model_selection import train_test_split
 
@@ -17,85 +19,21 @@ logger = logging.getLogger("__PREPROCESSING__")
 
 
 
-def load_data(images_dir: str,
-              masks_dir: str,
-              img_shape: Tuple[int, int] = None) -> Tuple[np.ndarray, np.ndarray]:
-    
-    """
-    Loads images and masks from directories and load into numpy array.
-
-    Args:
-        
-        images_dir (str): Path to the directory of the images.
-        masks_dir (str): Path to the directory of the masks.
-        img_shape (tuple): Shape of the loaded images.
-    
-    Returns:
-        Tuples of array contains the images and masks.
-
-    """
-
-    # wrangle the dataframe.
-    df = wrangle_df(masks_dir)
-
-    images = []
-    masks = []
-
-    # loof through each row in the dataframe.
-    for _, row in df.iterrows():
-        
-        img_path = f"{images_dir}/{row.ImageId}"
-        # load the image
-        image = tf.keras.preprocessing.image.load_img(img_path, target_size=img_shape)
-        # convert the loaded image to numpy array.
-        image = tf.keras.preprocessing.image.img_to_array(image)
-
-        # check if images  contains encoded pixels.
-        if not pd.isna(row.EncodedPixels):
-            # decode the RLE encoded pixels and create a mask.
-            mask = rle2mask(row.EncodedPixels, image.shape)
-        else:
-            # create an empty mask with the same image size
-            mask = np.zeros((image.shape[0], image.shape[1]), dtype=np.uint8)
-
-        # append to the list.
-        images.append(image)
-        masks.append(mask)
-
-    return np.array(images), np.array(masks)
-
-
-def split_dataset(images: np.ndarray, masks: np.ndarray) -> Tuple[tf.data.Dataset, tf.data.Dataset]:
+def split_dataset(df: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
     """
     Splits the datasets into training and validation sets.
 
     Args:
-        images (np.ndarray): Numpy array containing the image data.
-        masks (np.ndarray): Numpy array containing the mask data.
+        df (pd.DataFrame)
 
     Return:
-        train_dataset (tf.data.Dataset): Trining dataset.
-        val_dataset (tf.data.Dataset): Validation dataset.
+        train_df (pd.DataFrame): Trining dataframe.
+        val_df (pd.DataFrame): Validation dataframe
     """
     # split into training and validation sets
-    X_train, X_val, y_train, y_val = train_test_split(images, masks, test_size=0.2, random_state=42)
+    train_df, val_df = train_test_split(df, test_size=0.2, random_state=42)
 
-    # create tensorflow datasets
-    train_dataset = tf.data.Dataset.from_tensor_slices((X_train, y_train))
-    val_dataset = tf.data.Dataset.from_tensor_slices((X_val, y_val))
-
-    return train_dataset, val_dataset
-
-
-
-def preprocess(image: np.ndarray, mask: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
-    """Preprocesses the image and mask."""
-
-    # convert the image and mask into float32.
-    image = tf.image.convert_image_dtype(image, tf.float32)
-    mask = tf.image.convert_image_dtype(mask, tf.float32)
-
-    return image, mask
+    return train_df, val_df
 
 
 def data_augmentation(image, mask):
@@ -121,8 +59,54 @@ def data_augmentation(image, mask):
     return image, mask
 
 
+class DataGenerator(Sequence):
+    """Custom data generator"""
+    def __init__(self, dataframe, image_dir, batch_size, image_size=(256, 256), **kwargs):
+        super().__init__(**kwargs)
+        self.dataframe = dataframe
+        self.image_dir = image_dir
+        self.batch_size = batch_size
+        self.image_size = image_size
+        self.on_epoch_end()
 
-def run(images_dir: str, masks_dir: str, img_shape: Tuple[int, int], batch: int = 8):
+    def __len__(self):
+        return int(np.floor(len(self.dataframe) / self.batch_size))
+
+    def __getitem__(self, index):
+        batch_df = self.dataframe.iloc[index * self.batch_size:(index + 1) * self.batch_size]
+        images, masks = self.__data_generation(batch_df)
+        return images, masks
+
+    def on_epoch_end(self):
+        self.dataframe = self.dataframe.sample(frac=1).reset_index(drop=True)
+
+    def __data_generation(self, batch_df):
+        images = np.empty((self.batch_size, *self.image_size, 3), dtype=np.float32)
+        masks = np.empty((self.batch_size, *self.image_size, 1), dtype=np.float32)
+
+        for i, (_, row) in enumerate(batch_df.iterrows()):
+            image_path = f"{self.image_dir}/{row.ImageId}"
+            image = cv.imread(image_path)
+            
+            if not pd.isna(row.EncodedPixels):
+                mask = rle2mask(row.EncodedPixels, image.shape[:2])
+            else:
+                mask = np.zeros((image.shape[0], image.shape[1]), dtype=np.uint8)
+            
+            image = cv.resize(image, self.image_size)
+            image = image / 255.0
+            
+            mask = cv.resize(mask, self.image_size, interpolation=cv.INTER_NEAREST)
+            mask = np.expand_dims(mask, axis=-1)
+
+            images[i,] = image
+            masks[i,] = mask
+
+        return images, masks
+
+
+
+def run(images_dir: str, masks_dir: str, batch: int, img_shape: Tuple[int, int]):
     """
     A main function that runs all the data preprocessing for image segmentation.
 
@@ -130,41 +114,35 @@ def run(images_dir: str, masks_dir: str, img_shape: Tuple[int, int], batch: int 
         images_dir (str): Path to the images directory.
         masks_dir (str): Path to the masks directory.
         img_shape (tuple): Desired shape for the loaded images.
-        batch (int): Batch size for training and validation datasets.
 
     Returns:
         train_dataset
         val_dataset
     """
 
-    # Load images and masks.
-    images, masks = load_data(images_dir, masks_dir)
-    logger.info("Completed loaded images and masks from path into numpy arrays.")
+    # wrangle the dataframe.
+    df = wrangle_df(masks_dir)
 
     # split data into training and validation sets.
-    train_dataset, val_dataset = split_dataset(images, masks)
+    train_df, val_df = split_dataset(df)
     logger.info("Split the dataset into training and validation sets.")
 
-    # preprocess and augment the training data.
-    train_dataset = train_dataset.map(preprocess)
-    # apply augmentation to the training datasets.
-    train_dataset = train_dataset.map(lambda image, mask: (data_augmentation(image, mask)))
+    # preprocess the training and validation data.
+    train_dataset = DataGenerator(train_df, images_dir, batch, img_shape) 
+    val_dataset = DataGenerator(val_df, images_dir, batch, img_shape)
+    logger.info("Completed the preprocessing of training and validation datasets.")
    
-    # preprocess the validation data.
-    val_dataset = val_dataset.map(preprocess)
-    logger.info("Completed the preprocessing and augmentation of the images and masks datasets.")
-
     # save training and validation dataset.
     save_tfrecord(train_dataset, '/mnt/data/train.tfrecord')
     save_tfrecord(val_dataset, '/mnt/data/val.tfrecord')
     logger.info("Successfully saved the training and validation datasets in TFRecord format.")
 
 
-
 def opt_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("--images-dir", type=str, required=True, help="Path to the train directory.")
     parser.add_argument("--masks-dir", type=str, required=True, help="Path to the mask directory.")
+    parser.add_argument("--batch", type=int, default=8, help="Number of batch size.")
     parser.add_argument("--img-shape", type=tuple, default=(640, 640), help="Image shape")
 
     return parser.parse_args()
@@ -176,5 +154,6 @@ if __name__ == "__main__":
 
     run(args.images_dir, 
         args.masks_dir, 
+        args.batch,
         args.img_shape
     )
