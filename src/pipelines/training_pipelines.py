b90dd51ee7ba237554142167a12d1af2d2839dc9
ldebele <lemidebele@gmail.com>
2024-07-21 03:52:50 +0300

    fix: training pipeline

--- src/pipelines/training_pipelines.py
+++ src/pipelines/training_pipelines.py
@@ -1,8 +1,14 @@
+import os
+
 from datetime import datetime
+from docker.types import Mount
 
 from airflow import DAG
 from airflow.providers.docker.operators.docker import DockerOperator 
 
+
+
+PWD = os.getcwd()
 
 
 with DAG(
@@ -13,42 +19,47 @@ with DAG(
     
     # Task 1: Data Ingestion
     data_ingestion = DockerOperator(
-        image="",
-        command="./download_dataset.sh airbus-ship-detection competitions",
         task_id="data_ingestion",
-        volume_mounts=[
-            "~/.kaggle:/root/.kaggle",
-            "${pwd}/data:/mnt/data"
+        image='airbus-test:latest',
+        # command="./download_dataset.sh airbus-ship-detection competitions",
+        command="python test.py",
+        mounts=[
+            Mount(source=f"{PWD}/.kaggle", target="/mnt/data", type="bind"),
+            Mount(source=f"{PWD}/data", target="/mnt/data", type="bind")
         ]
     )
 
     # Task 2: Data Preprocessing
     preprocessing = DockerOperator(
-        image="",
-        command="python3 preprocess.py --images-dir='/mnt/data/images' --masks-dir='/mnt/data/mask', --img-shape=(768, 768, 3)",
         task_id="preprocessing",
-        volume_mounts=[
-            "/data:/mnt/data"
+        image="airbus-test:latest",
+        # command="python3 preprocess.py --images-dir='/mnt/data/images' --masks-dir='/mnt/data/mask', --img-shape=(768, 768, 3)",
+        command="python test.py",
+        mounts=[
+            Mount(source=f"{PWD}/data", target="/mnt/data", type="bind")
         ]
     )
 
     # Task 3: Model Training
     model_training = DockerOperator(
-        image="",
-        command="python3 train.py --train-dir='/mnt/data/train.tfrecord' --val-dir='/mnt/data/val.tfrecord' --num-classes=1, --img-shape=(768, 768, 3) --batch=8, --epochs=50 --learning-rate=0.001",
         task_id="model_training",
-        volume_mounts=[
-            "/data/:/mnt/data/"
+        image="airbus-test:latest",
+        # command="python3 train.py --train-dir='/mnt/data/train.tfrecord' --val-dir='/mnt/data/val.tfrecord' --num-classes=1, --img-shape=(768, 768, 3) --batch=8, --epochs=50 --learning-rate=0.001",
+        command="python test.py",
+        mounts=[
+            Mount(source=f"{PWD}/data", target="/mnt/data", type="bind"),
+            Mount(source=f"{PWD}/outputs", target="/mnt/data/outputs", type="bind")
         ]
     )
 
     # Task 4: Model Evaluation
     model_evaluation = DockerOperator(
-        image="",
-        command="python3 evaluation.py --test-dir='/mnt/data/' --model-dir='/mnt/data/models' --target-size=(768. 768) --batch=8",
         task_id="model_evaluation",
-        volume_mounts=[
-            "/data/test:/mnt/data/test"
+        image="airbus-test:latest",
+        # command="python3 evaluation.py --test-dir='/mnt/data/' --model-dir='/mnt/data/models' --target-size=(768. 768) --batch=8",
+        command="python test.py",
+        mounts=[
+            Mount(source=f"{PWD}/data/test", target="/mnt/data/test", type="bind")
         ]
     )
 
