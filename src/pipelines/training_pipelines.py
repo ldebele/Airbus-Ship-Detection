e5fa87209211bf98cba2173e74ef3424c9d77de4
ldebele <lemidebele@gmail.com>
2024-07-22 03:21:30 +0300

    fix training pipeline

--- src/pipelines/training_pipelines.py
+++ src/pipelines/training_pipelines.py
@@ -1,14 +1,18 @@
 import os
 
 from datetime import datetime
+from dotenv import load_dotenv
+
 from docker.types import Mount
 
 from airflow import DAG
 from airflow.providers.docker.operators.docker import DockerOperator 
 
 
+load_dotenv()
 
-PWD = os.getcwd()
+
+ROOT_DIR = os.getenv("ROOT_DIR", os.getcwd())
 
 
 with DAG(
@@ -20,47 +24,61 @@ with DAG(
     # Task 1: Data Ingestion
     data_ingestion = DockerOperator(
         task_id="data_ingestion",
-        image='airbus-test:latest',
-        # command="./download_dataset.sh airbus-ship-detection competitions",
-        command="python test.py",
+        image='ship-detection-data-ingestion:latest',
+        command="./download_dataset.sh airbus-ship-detection competitions",
         mounts=[
-            Mount(source=f"{PWD}/.kaggle", target="/mnt/data", type="bind"),
-            Mount(source=f"{PWD}/data", target="/mnt/data", type="bind")
-        ]
+            Mount(source=f"{ROOT_DIR}/.kaggle", target="/app/.kaggle", type="bind"),
+            Mount(source=f"{ROOT_DIR}/data", target="/mnt/data", type="bind")
+        ],
+        docker_url='tcp://docker-proxy:2375',
     )
 
     # Task 2: Data Preprocessing
     preprocessing = DockerOperator(
         task_id="preprocessing",
-        image="airbus-test:latest",
-        # command="python3 preprocess.py --images-dir='/mnt/data/images' --masks-dir='/mnt/data/mask', --img-shape=(768, 768, 3)",
-        command="python test.py",
+        image="ship-detection-preprocess:latest",
+        command="python3 preprocess.py \
+                --images-dir='/mnt/data/images' \
+                --masks-dir='/mnt/data/mask' \
+                --img-shape=(768, 768, 3)",
         mounts=[
-            Mount(source=f"{PWD}/data", target="/mnt/data", type="bind")
-        ]
+            Mount(source=f"{ROOT_DIR}/data", target="/mnt/data", type="bind")
+        ],
+        docker_url='tcp://docker-proxy:2375',
     )
 
     # Task 3: Model Training
     model_training = DockerOperator(
         task_id="model_training",
-        image="airbus-test:latest",
-        # command="python3 train.py --train-dir='/mnt/data/train.tfrecord' --val-dir='/mnt/data/val.tfrecord' --num-classes=1, --img-shape=(768, 768, 3) --batch=8, --epochs=50 --learning-rate=0.001",
-        command="python test.py",
+        image="ship-detection-training:latest",
+        command="python3 train.py \
+                --train-dir='/mnt/data/train.tfrecord' \
+                --val-dir='/mnt/data/val.tfrecord' \
+                --num-classes=1 \
+                --img-shape=(768, 768, 3) \
+                --batch=8 \
+                --epochs=50 \
+                --learning-rate=0.001",
         mounts=[
-            Mount(source=f"{PWD}/data", target="/mnt/data", type="bind"),
-            Mount(source=f"{PWD}/outputs", target="/mnt/data/outputs", type="bind")
-        ]
+            Mount(source=f"{ROOT_DIR}/data", target="/mnt/data", type="bind"),
+            Mount(source=f"{ROOT_DIR}/outputs", target="/mnt/outputs", type="bind")
+        ],
+        docker_url='tcp://docker-proxy:2375',
     )
 
     # Task 4: Model Evaluation
     model_evaluation = DockerOperator(
         task_id="model_evaluation",
-        image="airbus-test:latest",
-        # command="python3 evaluation.py --test-dir='/mnt/data/' --model-dir='/mnt/data/models' --target-size=(768. 768) --batch=8",
-        command="python test.py",
+        image="ship-detection-evaluation:latest",
+        command="python3 evaluation.py \
+                --test-dir='/mnt/data/' \
+                --model-dir='/mnt/data/models' \
+                --target-size=(768. 768) \
+                --batch=8",
         mounts=[
-            Mount(source=f"{PWD}/data/test", target="/mnt/data/test", type="bind")
-        ]
+            Mount(source=f"{ROOT_DIR}/data/test", target="/mnt/data/test", type="bind")
+        ],
+        docker_url='tcp://docker-proxy:2375',
     )
 
     # set task dependencies
